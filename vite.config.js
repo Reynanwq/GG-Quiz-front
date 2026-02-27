9416b79f13ffad4112ce9f20aeccc7abfbf19ea0
reynanwq <reynanwq@gmail.com>
2026-02-27 19:58:07 -0300

    fix: add specific allowed host for Render

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: "all", // 👈 PERMITE QUALQUER HOST
+    allowedHosts: ["gg-quiz-front.onrender.com"], // 👈 USANDO ARRAY
   },
 });
